06388a2266992d10527c12dcd9413bea4a82b1cc
Pedro J. Saba <pedrojsaba@gmail.com>
2017-09-26 15:56:30 -0500

    start project#

---
+++ application/controllers/Fibonacci.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Fibonacci
+ *
+ * @author Administrador
+ */
+class Fibonacci extends CI_Controller {
+
+    public $fibonacciCache = array(20);
+
+    public function exponencial($id) {
+        ini_set('max_execution_time', 0);
+        ini_set('memory_limit', '2048M');
+        echo "********** Complexity: O(2^N) **********" . "<br>";
+        $starttime = microtime(true);
+        echo 'Resultado: ' . $this->fibonacci1($id) . "<br>";
+        $endtime = microtime(true);
+        $time_milli = (int) round(($endtime - $starttime) * 1000);        
+        echo "Tiempo: " . number_format(($endtime - $starttime), 20, '.', '') . "<br>";
+    }
+
+    public function lineal1($id) {
+        echo "********** Complexity: O(N) **********" . "<br>";
+        $starttime = microtime(true);
+        echo 'Resultado: ' . $this->fibonacci2($id) . "<br>";
+        $endtime = microtime(true);
+        $time_milli = (int) round(($endtime - $starttime) * 1000);        
+        echo "Tiempo: " . number_format(($endtime - $starttime), 20, '.', '') . "<br>";
+    }
+
+    public function lineal2($id) {
+        echo "********** Complexity: O(N) **********" . "<br>";
+        $starttime = microtime(true);
+        echo 'Resultado: ' . $this->fibonacci3($id) . "<br>";
+        $endtime = microtime(true);
+        $time_milli = (int) round(($endtime - $starttime) * 1000);        
+        echo "Tiempo: " . number_format(($endtime - $starttime), 20, '.', '') . "<br>";
+    }
+
+    /// Complexity: O(2^N)
+    private function fibonacci1($n) {
+        if ($n < 0) {
+            throw new Exception("N can not be less than zero");
+        }
+        if ($n <= 2) {
+            return 1;
+        }
+        return $this->fibonacci1($n - 1) + $this->fibonacci1($n - 2);
+    }
+
+    /// Complexity: O(N)
+    private function fibonacci2($n) {
+        if ($n < 0) {
+            throw new Exception("N can not be less than zero");
+        }
+        if ($n <= 2) {
+            return 1;
+        }
+        $fibonacci = 0;
+        $previous = 1;
+        $penultimate = 0;
+        for ($i = 1; $i <= $n; $i++) {
+            $penultimate = $fibonacci;
+            $fibonacci = $previous + $fibonacci;
+            $previous = $penultimate;
+        }
+        return number_format($fibonacci, 0, '', '');
+    }
+
+    /// Complexity: O(N)
+    private function fibonacci3($n) {       
+        if (count($this->fibonacciCache) == 1) {            
+            $this->fibonacciCache = $this->init($n);
+        }
+        if ($n < 0) {
+            throw new Exception("N can not be less than zero");
+        }
+
+        if ($n <= 2) {
+            $this->fibonacciCache[$n] = 1;
+        }
+
+        if ($this->fibonacciCache[$n] == 0) {
+            $this->fibonacciCache[$n] = $this->fibonacci3($n - 1) + $this->fibonacci3($n - 2);
+        }
+        return $this->fibonacciCache[$n];
+    }
+
+    private function init($n) {
+        $arr = array();
+        for ($i = 0; $i <= $n; $i++) {
+            $arr[$i] = 0;
+        }
+        return $arr;
+    }
+
+}
